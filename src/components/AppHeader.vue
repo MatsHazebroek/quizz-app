3b6fee1368f785d29088fa1c1976c2dff43cc5c6
MatsHazebroek <matshazebroek@gmail.com>
2023-06-22 00:36:31 +0200

    Init Tailwindcss

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+      <nav class=" py-2 md:py-4">
+        <div class="container px-4 mx-auto md:flex md:items-center">
+          <div class="flex justify-between items-center">
+            <a href="/" class="font-bold text-xl text-blue-500">
+              Quizz App
+            </a>
+          </div>
+
+          <div class="flex md:flex flex-col md:flex-row md:ml-auto mt-3 md:mt-0">
+            <a
+              href="/"
+              class="p-2 lg:px-4 md:mx-2 text-white rounded bg-blue-500"
+            >
+              Home
+            </a>
+            <a
+              href="/"
+              class="p-2 lg:px-4 md:mx-2 text-gray-600 rounded hover:bg-gray-200 hover:text-gray-700 transition-colors duration-300"
+            >
+              Speel
+            </a>
+
+          </div>
+        </div>
+      </nav>
+      <hr class="border" />
+    </div>
+</template>
+
+<style scoped></style>
